19fd669b80f029215a49df137d28513aa4777eb8
Shiv Dubey <shiv.dubey@gaadi.com>
2022-02-28 23:06:32 +0530

    initial setup after deleting auto generated files

---
+++ vastram/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat
+      voluptates quisquam deleniti molestiae eveniet vitae exercitationem
+      voluptatibus modi, odio commodi corporis tenetur, consectetur ut nam
+      minima eum maiores? Quia, ad!
+    </div>
+  );
+}
+
+export default App;
