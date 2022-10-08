f9fd413ea84e7a3ef6e9acd954b1bc7215615e79
Shiv Dubey <shivdubey.dev@gmail.com>
2022-10-08 13:09:23 +0530

    include mui and enable support for changing themes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,40 @@
+import React, { useState } from "react";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import createEmotionCache from "../utility/createEmotionCache";
+import lightTheme from "../styles/theme/lightTheme";
+import darkTheme from "../styles/theme/darkTheme";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const themes = { light: lightTheme, dark: darkTheme };
+
+const ClientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = ClientSideEmotionCache,
+}) {
+  const [theme, setTheme] = useState(lightTheme);
+
+  const changeTheme = (_theme) => {
+    if (Object.keys(themes).includes(_theme)) setTheme(themes[_theme]);
+    else
+      console.error(
+        "A component is trying to set a theme which is not defined",
+        _theme
+      );
+  };
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} changeTheme={changeTheme} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
 
 export default MyApp;
